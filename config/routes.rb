6a480bfe24228d27b2a5672410a6afb8deb7d8b9
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2021-09-14 14:09:08 +0200

    api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :task_types
+  namespace :api do
+    namespace :v1 do
+      resources :task_types
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
